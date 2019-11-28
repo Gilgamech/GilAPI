bbe3e330f02d44d7970ac156d5caab0620626f97
GILNOVOX\Gilgamech <Gilgamech@Gilgamech.com>
2019-11-28 08:16:36 -0800

    Add CORS headers.

--- index.js
+++ index.js
@@ -140,7 +140,11 @@ app.use(session({
 	saveUninitialized: true, 
 	maxAge: null
 }));
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
